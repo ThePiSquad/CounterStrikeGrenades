2a6502754660253a66d665fc29bef40f597ac684
Kening Hu <kening.hu@outlook.com>
2024-08-05 23:12:59 +0800

    first commit

---
+++ src/main/kotlin/club/pisquad/minecraft/csgrenades/CounterStrikeGrenades.kt
@@ -0,0 +1,43 @@
+package club.pisquad.minecraft.csgrenades
+
+import net.minecraftforge.fml.common.Mod
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent
+import net.minecraftforge.fml.event.lifecycle.FMLDedicatedServerSetupEvent
+import org.apache.logging.log4j.Level
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+
+/**
+ * Main mod class. Should be an `object` declaration annotated with `@Mod`.
+ * The modid should be declared in this object and should match the modId entry
+ * in mods.toml.
+ *
+ * An example for blocks is in the `blocks` package of this mod.
+ */
+@Mod(CounterStrikeGrenades.ID)
+object CounterStrikeGrenades {
+    const val ID = "csgrenades"
+
+    // the logger for our mod
+    val LOGGER: Logger = LogManager.getLogger(ID)
+
+    init {
+        LOGGER.log(Level.INFO, "Hello world!")
+    }
+
+    /**
+     * This is used for initializing client specific
+     * things such as renderers and keymaps
+     * Fired on the mod specific event bus.
+     */
+    private fun onClientSetup(event: FMLClientSetupEvent) {
+        LOGGER.log(Level.INFO, "Initializing client...")
+    }
+
+    /**
+     * Fired on the global Forge bus.
+     */
+    private fun onServerSetup(event: FMLDedicatedServerSetupEvent) {
+        LOGGER.log(Level.INFO, "Server starting...")
+    }
+}
