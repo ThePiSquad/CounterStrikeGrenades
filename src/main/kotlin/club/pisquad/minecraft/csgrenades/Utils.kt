70d0f89f24df9262b9c13a0f134653396f794b9d
Kening Hu <kening.hu@outlook.com>
2024-08-11 23:10:04 +0800

    fix possible randomSource issue

--- src/main/kotlin/club/pisquad/minecraft/csgrenades/Utils.kt
+++ src/main/kotlin/club/pisquad/minecraft/csgrenades/Utils.kt
@@ -1,8 +1,8 @@
 package club.pisquad.minecraft.csgrenades
 
 import net.minecraft.core.Vec3i
+import net.minecraft.util.RandomSource
 import net.minecraft.world.phys.Vec3
-import kotlin.random.Random
 
 /**
  *Since KFF is not mapping those methods correctly
@@ -17,11 +17,12 @@ fun getTimeFromTickCount(tickCount: Double): Double {
 }
 
 fun getRandomLocationFromSphere(center: Vec3, radius: Double): Vec3 {
+    val randomSource = RandomSource.create()
     while (true) {
         val posDelta = Vec3(
-            Random.nextDouble(0.0, radius * 2) - radius,
-            Random.nextDouble(0.0, radius * 2) - radius,
-            Random.nextDouble(0.0, radius * 2) - radius
+            randomSource.nextDouble() * radius * 2 - radius,
+            randomSource.nextDouble() * radius * 2 - radius,
+            randomSource.nextDouble() * radius * 2 - radius
         )
         if (posDelta.length() < radius) {
             return center.add(posDelta)
